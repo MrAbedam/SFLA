d3a1c1270bbe77e12a9c4832b80428a7ddbf4d82
mmd <1mmmlabd@gmail.com>
2025-02-06 00:58:03 +0330

    Add matlab files

--- Final System C3/solution_comparison_matlab.m
+++ Final System C3/solution_comparison_matlab.m
@@ -11,9 +11,6 @@ fclose(fileID);
 Ug_Fitness = data{2}(1);
 Us_Fitness = data{2}(2);
 
-
-numItems = length(Ug_Binary);
-
 figure;
 bar([Ug_Fitness, Us_Fitness]);
 set(gca, 'xticklabel', {'Ug', 'Us'});
